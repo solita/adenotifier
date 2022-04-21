cab8ce14ae22f82e72e6e0630bb602545860d988
tahaga <taha.gad@solita.fi>
2022-04-21 16:25:30 +0300

    setup changes

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ade_notifier',
+    name='adenotifier',
     version='0.0.1',
     author='Taha Gad, Henri Hemminki',
     author_email='taha.gad@solita.fi, henri.hemminki@solita.fi',
     description='Python library for using ADE Notify API',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url='https://github.com/solita-internal/ade-notifier',
     project_urls = {
         "Bug Tracker": "https://github.com/solita-internal/ade-notifier/issues"
     },
     license='MIT',
-    packages=['manifest', 'functions'],
+    packages=['ade_manifest', 'ade_notifier'],
     install_requires=['requests']
 )
